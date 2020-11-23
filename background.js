7bd1a2d37b28859ba9c865f6dbddaf5168068d9d
Sebastian Raimondi <sebastianraimondi@gmail.com>
2020-11-22 21:36:56 -0300

    timeoute delete

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
+console.log("LOAD");
+
 const getChannel = () => window.location.href.split("/")[3];
 const buildURL = () =>
   `https://player.twitch.tv/?channel=${getChannel()}&parent=twitch.tv`;
 
+const deleteVideo = () => {
+  const video = document.querySelector("video");
+  if (video) video.remove();
+  console.log("deleted");
+};
+
 const playerClass = "video-player";
+const deleteTimeout = 20000;
 
 const videoplayer = document.getElementsByClassName(playerClass)[0];
 const container = videoplayer.parentElement;
@@ -11,7 +20,8 @@ const iframe = document.createElement("iframe");
 iframe.setAttribute("class", playerClass);
 iframe.setAttribute("src", buildURL());
 
-videoplayer.remove();
 container.append(iframe);
+
+setTimeout(deleteVideo, deleteTimeout);
 
 // const updateChannel = () => iframe.setAttribute("src", buildURL());
