c8bf2f926e4cbd72e7f2c29728725fd606e88cb2
Sebastian Raimondi <sebastianraimondi@gmail.com>
2020-11-22 22:39:04 -0300

    Pause on delete

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 console.log("LOAD");
 
+const playerClass = "video-player";
+const playClass =
+  "tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--overlay tw-core-button tw-core-button--overlay tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative";
+const deleteInterval = 100;
+
 const getChannel = () => window.location.href.split("/")[3];
 const buildURL = () =>
   `https://player.twitch.tv/?channel=${getChannel()}&parent=twitch.tv`;
@@ -13,11 +18,11 @@ const deleteVideo = () => {
     clearInterval(intervalID);
     video.remove();
     console.log("deleted");
-  } else console.log("False video", video);
-};
 
-const playerClass = "video-player";
-const deleteInterval = 20000;
+    const button = document.getElementsByClassName(playClass)[0];
+    if (button.ariaLabel.startsWith("Pause")) button.click();
+  }
+};
 
 const videoplayer = document.getElementsByClassName(playerClass)[0];
 const container = videoplayer.parentElement;
