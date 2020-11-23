e05bb38ad219f347cb6429f12a1686b8ef51d02b
Sebastian Raimondi <sebastianraimondi@gmail.com>
2020-11-22 22:13:13 -0300

    interval delete

--- background.js
+++ background.js
@@ -4,14 +4,20 @@ const getChannel = () => window.location.href.split("/")[3];
 const buildURL = () =>
   `https://player.twitch.tv/?channel=${getChannel()}&parent=twitch.tv`;
 
+let intervalID;
 const deleteVideo = () => {
   const video = document.querySelector("video");
-  if (video) video.remove();
-  console.log("deleted");
+
+  if (video) {
+    console.log("Video", video);
+    clearInterval(intervalID);
+    video.remove();
+    console.log("deleted");
+  } else console.log("False video", video);
 };
 
 const playerClass = "video-player";
-const deleteTimeout = 20000;
+const deleteInterval = 20000;
 
 const videoplayer = document.getElementsByClassName(playerClass)[0];
 const container = videoplayer.parentElement;
@@ -22,6 +28,7 @@ iframe.setAttribute("src", buildURL());
 
 container.append(iframe);
 
-setTimeout(deleteVideo, deleteTimeout);
+intervalID = setInterval(deleteVideo, deleteInterval);
+console.log("Interval", intervalID);
 
 // const updateChannel = () => iframe.setAttribute("src", buildURL());
